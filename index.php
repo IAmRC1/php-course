6d178c252fac14256c9833301380c0540b46e911
Rishabh Singh <rishabh@hestabit.in>
2020-05-04 22:22:24 +0530

    add variables

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <!DOCTYPE html>
 <html>
   <head>
@@ -6,24 +5,11 @@
   </head>
   <body>
     <?php
-    // Using two forward slashes we create comments in php.
-    // Within the php tag, we can insert values into our HTML.
-    echo "<h1>Welcome!</h1>";
+      $name = "PHP Store";
+      $credit = 1000; 
+
+      echo "<h1>Welcome to ".$name."!</h1>";
+      echo "<h2>You have $".$credit." in your wallet.</h2>";
     ?>
   </body>
 </html>
-=======
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>PHP Store</title>
-  </head>
-  <body>
-    <?php
-    // Using two forward slashes we create comments in php.
-    // Within the php tag, we can insert values into our HTML.
-    echo "<h1>Welcome!</h1>";
-    ?>
-  </body>
-</html>
->>>>>>> fd82fbddca96d4a3e1f5da0c756498e7375ffe24
